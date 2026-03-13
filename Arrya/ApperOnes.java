1264b8f04718c67cf0b679eaca2b7770be061223
Vaibhav Gutthe <gutthevaibhav0@gmail.com>
2026-03-13 14:44:02 +0530

    add more code init this are optimal solution of it

--- Arrya/ApperOnes.java
+++ Arrya/ApperOnes.java
@@ -13,12 +13,20 @@ class OnesApper{
             System.out.println(num+" Appers numbere only ones!");
         }
     }
-   }
+}
+    public void usingxor(int arr[]){
+        int xor1=0;
+        for(int i=0;i<arr.length;i++){
+            xor1=xor1^arr[i];
+        }
+        System.out.println("The number is appers only ones is:"+xor1);
+    }  
 }
 public class ApperOnes {
     public static void main(String[] args) {
         int arrr[]={1,2,2,3,1,5,8,5,6,9,8,3,6};
         OnesApper one=new OnesApper();
         one.appersone(arrr);
+        one.usingxor(arrr);
     }
 }
