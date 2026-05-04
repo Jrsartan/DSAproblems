309bfc093b41cf2988d8b4c00af085ba3ba897d6
Vaibhav Gutthe <gutthevaibhav0@gmail.com>
2026-05-05 03:13:27 +0530

    one change

--- Arrya/findNCR.java
+++ Arrya/findNCR.java
@@ -2,7 +2,7 @@ import java.util.*;
 class find_NCR{
     public void ncr_find_opti(int n,int r){
         int res=1;
-        for(int i=0;i<r;i++){
+        for(int i=0;i<r;i++){ 
             res=res*(n-i);
             res=res/(i+1);
         }
